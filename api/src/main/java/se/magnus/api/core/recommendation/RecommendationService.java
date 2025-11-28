36bb8968f203edbbf5e3712ddd0a04ee43c1a36a
serg10arg <cabrerasergio094@gmail.com>
2025-11-28 12:47:35 -0300

    feat(api): define contratos para servicios de product, recommen y review
    Se añaden las interfaces de servicio y los modelos de datos (DTOs) para
    los microservicios principales: Product, Recommendation y Review.
    
    Estos archivos establecen el contrato publico que cada microservicio
    expondra, definiendo los endpoints y las estructuras para la
    comunicacion entre servicios.

---
+++ api/src/main/java/se/magnus/api/core/recommendation/RecommendationService.java
@@ -0,0 +1,26 @@
+package se.magnus.api.core.recommendation;
+
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+public interface RecommendationService {
+
+    @PostMapping(
+            value = "/recommendation",
+            consumes = "application/json",
+            produces = "application/json")
+    Recommendation createRecommendation(@RequestBody Recommendation body);
+
+    @GetMapping(
+            value = "/recommendation",
+            produces = "application/json")
+    List<Recommendation> getRecommendations(
+            @RequestParam(value = "productId", required = true) int productId);
+
+    @DeleteMapping(
+            value = "/recommendation")
+    void deleteRecommendations(@RequestParam(value = "productId", required = true)int productId);
+}
