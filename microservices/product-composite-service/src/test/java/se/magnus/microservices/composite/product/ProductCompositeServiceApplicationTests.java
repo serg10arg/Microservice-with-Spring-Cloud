734d89eda8b5e2de9abdf91c42e5b2acefd8a5ed
serg10arg <cabrerasergio094@gmail.com>
2025-12-01 19:47:16 -0300

    test(product-composite): pruebas de integracion para servicio compuesto.
    Se implementa pruebas de integracion para el endpoint
    /product-composite/{productId}.
    
    Estas pruebas utilizan @MockitoBean para simular las respuestas de los
    servicios subyacentes (producto, recomendacion y reseña), permitiendo
    verificar el comportamiento del servicio compuesto de forma aislada.
    Se cubren los escenarios de exito (HTTP 200), producto no encontrado
    (HTTP 404) y la intrada invalida (HTTP 422).

--- microservices/product-composite-service/src/test/java/se/magnus/microservices/composite/product/ProductCompositeServiceApplicationTests.java
+++ microservices/product-composite-service/src/test/java/se/magnus/microservices/composite/product/ProductCompositeServiceApplicationTests.java
@@ -1,13 +1,102 @@
 package se.magnus.microservices.composite.product;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static java.util.Collections.singletonList;
+import static org.mockito.Mockito.when;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+import static org.springframework.http.HttpStatus.UNPROCESSABLE_ENTITY;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
 
-@SpringBootTest
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import se.magnus.api.core.product.Product;
+import se.magnus.api.core.recommendation.Recommendation;
+import se.magnus.api.core.review.Review;
+import se.magnus.api.exceptions.InvalidInputException;
+import se.magnus.api.exceptions.NotFoundException;
+import se.magnus.microservices.composite.product.services.ProductCompositeIntegration;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
 class ProductCompositeServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    private static final int PRODUCT_ID_OK = 1;
+    private static final int PRODUCT_ID_NOT_FOUND = 2;
+    private static final int PRODUCT_ID_INVALID = 3;
 
+    @Autowired private WebTestClient client;
+
+    @MockitoBean private ProductCompositeIntegration compositeIntegration;
+
+    @BeforeEach
+    void setUp() {
+
+        when(compositeIntegration.getProduct(PRODUCT_ID_OK))
+                .thenReturn(new Product(PRODUCT_ID_OK, "name", 1, "mock-address"));
+        when(compositeIntegration.getRecommendations(PRODUCT_ID_OK))
+                .thenReturn(singletonList(new Recommendation(PRODUCT_ID_OK, 1, "author", 1, "content", "mock address")));
+        when(compositeIntegration.getReviews(PRODUCT_ID_OK))
+                .thenReturn(singletonList(new Review(PRODUCT_ID_OK, 1, "author", "subject", "content", "mock address")));
+
+        when(compositeIntegration.getProduct(PRODUCT_ID_NOT_FOUND))
+                .thenThrow(new NotFoundException("NOT FOUND: " + PRODUCT_ID_NOT_FOUND));
+
+        when(compositeIntegration.getProduct(PRODUCT_ID_INVALID))
+                .thenThrow(new InvalidInputException("INVALID: " + PRODUCT_ID_INVALID));
+    }
+
+    @Test
+    void contextLoads() {}
+
+    @Test
+    void getProductById() {
+
+        client.get()
+                .uri("/product-composite/" + PRODUCT_ID_OK)
+                .accept(APPLICATION_JSON)
+                // 1. Envía la petición
+                .exchange()
+                // 2. Verifica los metadatos: ¿El código de estado es 200 OK?
+                .expectStatus().isOk()
+                // 3. Verifica los metadatos: ¿La cabecera 'Content-Type' es 'application/json'?
+                .expectHeader().contentType(APPLICATION_JSON)
+                // 4. Ahora, abre el cuerpo de la respuesta para inspeccionar su contenido.
+                .expectBody()
+                // 5. Dentro del cuerpo JSON, verifica que el campo 'productId' sea igual a 1.
+                .jsonPath("$.productId").isEqualTo(PRODUCT_ID_OK)
+                // 6. Verifica que la lista 'recommendations' tenga 1 elemento.
+                .jsonPath("$.recommendations.length()").isEqualTo(1)
+                // 7. Verifica que la lista 'reviews' tenga 1 elemento.
+                .jsonPath("$.reviews.length()").isEqualTo(1);
+    }
+
+    @Test
+    void getProductNotFound() {
+
+        client.get()
+                .uri("/product-composite/" + PRODUCT_ID_NOT_FOUND)
+                .accept(APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isNotFound()
+                .expectHeader().contentType(APPLICATION_JSON)
+                .expectBody()
+                .jsonPath("$.path").isEqualTo("/product-composite/" + PRODUCT_ID_NOT_FOUND)
+                .jsonPath("$.message").isEqualTo("NOT FOUND: " + PRODUCT_ID_NOT_FOUND);
+    }
+
+    @Test
+    void getProductInvalidInput() {
+
+        client.get()
+                .uri("/product-composite/" + PRODUCT_ID_INVALID)
+                .accept(APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isEqualTo(UNPROCESSABLE_ENTITY)
+                .expectHeader().contentType(APPLICATION_JSON)
+                .expectBody()
+                .jsonPath("$.path").isEqualTo("/product-composite/" + PRODUCT_ID_INVALID)
+                .jsonPath("$.message").isEqualTo("INVALID: " + PRODUCT_ID_INVALID);
+    }
 }
